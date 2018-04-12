d680a584d737d0c93033a18540c5487bb7ee7a25
ecs90 <ecs_salas@hotmail.com>
2018-04-12 00:49:21 -0300

    db

--- database.sql
+++ database.sql
@@ -1,4 +1,9 @@
+create database if not exists tpJava;
+
+use tpJava;
+
 CREATE TABLE winners (
+  id int not null auto_increment primary key;
   player VARCHAR(30) NOT NULL,
   points INT NOT NULL,
   cards VARCHAR(200) NOT NULL
